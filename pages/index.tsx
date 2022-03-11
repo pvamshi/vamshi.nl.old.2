c5aa615d7bcef1ccac60806693d1dab43497070c
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2022-03-11 12:42:19 +0530

    in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from "next";
-import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h2>home</h2>
-
-      <Link href="code">Code</Link>
+      <p>In progress...</p>
     </>
   );
 };
