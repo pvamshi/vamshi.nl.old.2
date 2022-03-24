4bc17a17930b67357f11a05a228dcf6f5a0cfa02
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2022-03-24 11:53:15 +0530

    update output to docs

---
+++ docs/_next/static/xSKboObKpbGuHbUsvdVOL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-814c7623891386a4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/code":["static/chunks/pages/code-bf114d8f8b02e45a.js"],sortedPages:["/","/_app","/_error","/code"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
